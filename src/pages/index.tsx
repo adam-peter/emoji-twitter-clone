06ee75996399500acf140327eaa2103b63daae4f
badmood111 <petrzela.adam@seznam.cz>
2023-04-16 14:32:21 +0200

    setup routes for posts and profiles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 import toast from "react-hot-toast";
 import { SignInButton, useUser } from "@clerk/nextjs";
@@ -82,17 +83,23 @@ const PostView = (props: PostWithUser) => {
 
   return (
     <div className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        src={author.profileImageUrl}
-        alt="profile-picture"
-        className="h-14 w-14 rounded-full"
-        width="56"
-        height="56"
-      />
+      <Link href={`/@${author.username}`}>
+        <Image
+          src={author.profileImageUrl}
+          alt="profile-picture"
+          className="h-14 w-14 rounded-full"
+          width="56"
+          height="56"
+        />
+      </Link>
       <div className="flex flex-col">
         <div className="flex whitespace-pre-wrap text-slate-300">
-          <span className="font-bold">{`@${author.username}`}</span>
-          <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span className="font-bold">{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
